2bda90b6a0586b29e78ecf297f05e21be52b3305
Asher Pinson <ashpinson@gmail.com>
2023-09-21 17:03:13 -0400

    🔧 config: Set up openapps.sh for Godot

--- openapps.sh
+++ openapps.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
 first_letter="$(echo $1 | head -c1)"
-godot="/home/superspacehero/.local/share/godot/app_userdata/Godots/versions/Godot_v4_1_1-stable_linux_x86_64/Godot_v4.1.1-stable_linux.x86_64" "--path"
-project_path="/home/superspacehero/Projects/Armored Harrier"
-scene="World.godot"
+godot="/home/superspacehero/.local/share/godot/app_userdata/Godots/versions/Godot_v4_1_1-stable_linux_x86_64/Godot_v4.1.1-stable_linux.x86_64  \"--path\""
+project_path="/home/superspacehero/Projects/Armored Harrier\" -e"
 github="flatpak run io.github.shiftey.Desktop & git lfs pull"
 
 case "$first_letter" in
     "p")
-        $godot "--path" $project_path "-e" & code . & $github &
+        $godot $project_path & code . & $github &
         ;;
     "b")
-        $godot "--path" $project_path "-e" & flatpak run org.blender.Blender & $github &
+        $godot $project_path & flatpak run org.blender.Blender & $github &
         ;;
     "a")
-        $godot "--path" $project_path "-e" & flatpak run org.gimp.GIMP & $github &
+        $godot $project_path & flatpak run org.gimp.GIMP & $github &
         ;;
     "c")
-        $godot "--path" $project_path "-e" & clip-snap-paint & $github &
+        $godot $project_path & clip-snap-paint & $github &
         ;;
     *)
-        $godot "--path" $project_path "-e" & $github &
+        $godot $project_path & $github &
         ;;
 esac
